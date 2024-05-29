63699d4358ab037a7ce734039bc54fca0de85976
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-05-29 16:35:33 -0700

    Final App

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,68 @@ namespace Prog122_S24_L13_RTBAdv
         {
             InitializeComponent();
         }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the title and paragraph content
+            string title = TitleTextBox.Text;
+            TextRange paragraphTextRange = new TextRange(ParagraphRichTextBox.Document.ContentStart, ParagraphRichTextBox.Document.ContentEnd);
+
+            // Get the font size
+            double fontSize = 12;
+            if (FontSizeComboBox.SelectedItem != null)
+            {
+                string selectedFontSize = (FontSizeComboBox.SelectedItem as ComboBoxItem).Content.ToString();
+                fontSize = double.Parse(selectedFontSize);
+            }
+
+            // Get the text style
+            FontWeight fontWeight = FontWeights.Normal;
+            FontStyle fontStyle = FontStyles.Normal;
+            TextDecorationCollection textDecorations = null;
+
+            if (ItalicRadioButton.IsChecked == true)
+            {
+                fontStyle = FontStyles.Italic;
+            }
+            if (BoldRadioButton.IsChecked == true)
+            {
+                fontWeight = FontWeights.Bold;
+            }
+            if (UnderlineRadioButton.IsChecked == true)
+            {
+                textDecorations = TextDecorations.Underline;
+            }
+
+            // Create the formatted text block
+            Paragraph formattedParagraph = new Paragraph();
+
+            // Add title
+            if (!string.IsNullOrEmpty(title))
+            {
+                Run titleRun = new Run(title)
+                {
+                    FontWeight = fontWeight,
+                    FontSize = fontSize,
+                    FontStyle = fontStyle,
+                    TextDecorations = textDecorations
+                };
+                formattedParagraph.Inlines.Add(titleRun);
+                formattedParagraph.Inlines.Add(new LineBreak());
+            }
+
+            // Add paragraph text
+            Run paragraphRun = new Run(paragraphTextRange.Text)
+            {
+                FontWeight = fontWeight,
+                FontSize = fontSize,
+                FontStyle = fontStyle,
+                TextDecorations = textDecorations
+            };
+            formattedParagraph.Inlines.Add(paragraphRun);
+
+            // Add the formatted paragraph to the display RichTextBox
+            FormattedRichTextBox.Document.Blocks.Add(formattedParagraph);
+        }
     }
 }
